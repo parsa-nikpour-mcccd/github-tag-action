3fc62051ac5fb72f93d78951c393d4d8125ae2c6
parsa-nikpour-mcccd <parsa.nikpour@domail.maricopa.edu>
2023-09-11 09:48:01 -0700

    Comment out requirement for commit to include "merge"

--- entrypoint.sh
+++ entrypoint.sh
@@ -127,11 +127,11 @@ commit=$(git rev-parse HEAD)
 ## Make sure the commit message contains "merge" so we are not running the workflow on every push to master
 commit_message=$( git log --format=%B -n 1 $commit )
 lowered_commit_message=$(awk '{ print tolower($0) }' <<< $commit_message)
-if [[ $lowered_commit_message != *"merge"* ]]
-then
-    echo "Commit doesn't contain 'merge'"
-    exit 0
-fi
+#if [[ $lowered_commit_message != *"merge"* ]]
+#then
+#    echo "Commit doesn't contain 'merge'"
+#    exit 0
+#fi
 
 # skip if there are no new commits for non-pre_release
 if [ "$tag_commit" == "$commit" ]
